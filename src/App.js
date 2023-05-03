c3a0941604e9fca175a7464d689f2458ada72dbf
ramzzzes <ramaza.giorgi@gmail.com>
2023-05-03 19:40:23 +0400

    second commit

--- src/App.js
+++ src/App.js
@@ -1,34 +1,29 @@
+import { useState } from 'react';
 import './App.css';
-import Movie from './Movie/Movie';
-
+import { Population,Area } from './Movie/Country'; 
+ 
 function App() {
-
-  const movieList = [
-    {
-      moviePoster : 'moviePoster',
-      title : 'Superman',
-      releaseDate : '2012-05-09',
-      movieUrl : 'https://static.displate.com/857x1200/displate/2019-11-27/dd8566e09466a7a657835a5af8612d94_1f96abec68490e6896ec5eb546ac3419.jpg'
-    },
-    {
-      moviePoster : 'moviePosterSecond',
-      title : 'Batman',
-      releaseDate : '2016-05-09',
-      movieUrl : 'https://m.media-amazon.com/images/I/71kUTilIdiL._AC_SL1500_.jpg'
-    }
-  ]
+  const [name,setName] = useState('jonathan')
+ 
+  const changeValue = (name) => {
+    setName(name)
+  }
 
   return (
-    <div>
-       {movieList.map((m,index) => <Movie 
-            key={index}
-            title={m.title}
-            moviePoster={m.moviePoster} 
-            movieUrl={m.movieUrl}
-            releaseDate={m.releaseDate}
-            />)
-        }
-    </div>
+    <>
+      <p>{name}</p>  
+ 
+      <button onClick={() => changeValue('blue') } >
+        make blue
+      </button>
+
+      <Population 
+        name={name}
+        changeValue={changeValue}
+      />
+
+    </> 
+     
   );
 }
 
